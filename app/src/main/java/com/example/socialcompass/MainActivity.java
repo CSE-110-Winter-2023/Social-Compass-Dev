d8ad5dd591f5eeccbf6648a7dab6585b54c6a566
Abhishek Govindarasu <abhishekgovindarasu@gmail.com>
2023-02-06 16:24:07 -0800

    location direction, permissions request not done

--- app/src/main/java/com/example/socialcompass/MainActivity.java
+++ app/src/main/java/com/example/socialcompass/MainActivity.java
@@ -1,14 +1,101 @@
 package com.example.socialcompass;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
 
 public class MainActivity extends AppCompatActivity {
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private Location currentLocation;
+    private Location targetLocation;
+
+    private TextView tv;
+
+    private final int REQUEST_LOCATION_PERMISSION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = (TextView) findViewById(R.id.textView);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                currentLocation = location;
+
+                // If a target location has been set, update the angle
+                if (targetLocation != null) {
+                    updateAngle();
+                }
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+            }
+        };
+
+        // Request location updates
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestLocationPermission();
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+        // Set the target location
+        targetLocation = new Location("Ottawa");
+        targetLocation.setLatitude(45.0819);
+        targetLocation.setLongitude(-73.3329);
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @AfterPermissionGranted(REQUEST_LOCATION_PERMISSION)
+    public void requestLocationPermission(){
+        String[] perms = {android.Manifest.permission.ACCESS_FINE_LOCATION};
+        if (EasyPermissions.hasPermissions(this, perms)){
+            Toast.makeText(this, "Permission already granted", Toast.LENGTH_SHORT);
+        } else {
+            EasyPermissions.requestPermissions(this, "Please grant the location permission", REQUEST_LOCATION_PERMISSION, perms);
+        }
+
+    }
+
+    private void updateAngle() {
+        float angle = currentLocation.bearingTo(targetLocation);
+        Log.d("tag", String.valueOf(currentLocation.getLatitude()));
+        Log.d("tag", String.valueOf(currentLocation.getLongitude()));
+        ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) tv.getLayoutParams();
+        layoutParams.circleAngle = angle;
+        tv.setLayoutParams(layoutParams);
+        tv.setRotation(angle);
+    }
+
 }
