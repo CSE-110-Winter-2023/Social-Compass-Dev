f3f9bf805a77d94b7f6a0414a07cf0b5dcc7958c
Abhishek Govindarasu <abhishekgovindarasu@gmail.com>
2023-02-19 22:40:22 -0800

    Compass UI and Location Object comments

--- app/src/main/java/com/example/socialcompass/CompassLocationObject.java
+++ app/src/main/java/com/example/socialcompass/CompassLocationObject.java
@@ -1,3 +1,6 @@
+/**
+ * Represents a location object with a name, location, and UI controller for a compass app.
+ */
 package com.example.socialcompass;
 
 import android.location.Location;
@@ -7,6 +10,12 @@ public class CompassLocationObject {
     private Location location;
     private CompassUIController controller;
 
+    /**
+     * Constructor for creating a CompassLocationObject
+     * @param locationName name of the location
+     * @param location location object containing latitude and longitude
+     * @param controller controller object to handle updating UI for this location
+     */
     public CompassLocationObject(String locationName, Location location, CompassUIController controller) {
         this.locationName = locationName;
         this.location = location;
@@ -17,30 +26,57 @@ public class CompassLocationObject {
         }
     }
 
+    /**
+     * Returns the name of the location object.
+     * @return the name of the location object
+     */
     public String getLocationName() {
         return locationName;
     }
 
+    /**
+     * Sets the name of the location object.
+     * @param locationName the new name for the location object
+     */
     public void setLocationName(String locationName) {
         this.locationName = locationName;
     }
 
+    /**
+     * Returns the location object.
+     * @return the location object
+     */
     public Location getLocation() {
         return location;
     }
 
+    /**
+     * Sets the location object.
+     * @param location the new location for the location object
+     */
     public void setLocation(Location location) {
         this.location = location;
     }
 
+    /**
+     * Returns the UI controller for the compass.
+     * @return the UI controller for the compass
+     */
     public CompassUIController getController() {
         return controller;
     }
 
+    /**
+     * Sets the UI controller for the compass.
+     * @param controller the new UI controller for the compass
+     */
     public void setController(CompassUIController controller) {
         this.controller = controller;
     }
 
+    /**
+     * Updates the name of the location object with the current text in the UI controller's TextView.
+     */
     public void syncName() {
         this.locationName = controller.getTextView().getText().toString();
     }
--- app/src/main/java/com/example/socialcompass/CompassUIController.java
+++ app/src/main/java/com/example/socialcompass/CompassUIController.java
@@ -1,3 +1,11 @@
+/**
+ * The CompassUIController class is responsible for updating the UI for the compass. It takes
+ * the current device location and orientation angles, as well as the distance to the target
+ * location, and updates the position and rotation of the compass needle accordingly. It
+ * implements the UIController interface to provide a common way to update the UI for different
+ * types of views.
+ */
+
 package com.example.socialcompass;
 
 import android.widget.TextView;
@@ -9,6 +17,16 @@ public class CompassUIController implements UIController {
     private int distance;
     private TextView tv;
 
+    /**
+     * Constructor for the CompassUIController class. It initializes the instance variables for the
+     * location angle, orientation angle, distance, and the TextView used to display the compass
+     * needle.
+     *
+     * @param locAngle The location angle in degrees
+     * @param orientAngle The orientation angle in degrees
+     * @param distance The distance to the target location
+     * @param tv The TextView used to display the compass needle
+     */
     public CompassUIController(float locAngle, float orientAngle, int distance, TextView tv){
         this.locAngle = locAngle;
         this.orientAngle = orientAngle;
@@ -16,30 +34,66 @@ public class CompassUIController implements UIController {
         this.tv = tv;
     }
 
+    /**
+     * Sets the location angle to the given value.
+     *
+     * @param loc The new location angle in degrees
+     */
     public void setLocAngle(float loc){
         locAngle = loc;
     }
 
+    /**
+     * Returns the current location angle.
+     *
+     * @return The current location angle in degrees
+     */
     public float getLocAngle(){
         return this.locAngle;
     }
 
+    /**
+     * Sets the orientation angle to the given value.
+     *
+     * @param orientation The new orientation angle in degrees
+     */
     public void setOrient(float orientation){
         orientAngle = orientation;
     }
 
+
+    /**
+     * Returns the current orientation angle.
+     *
+     * @return The current orientation angle in degrees
+     */
     public float getOrient(){
         return this.orientAngle;
     }
 
+    /**
+     * Sets the TextView used to display the compass needle to the given TextView.
+     *
+     * @param tv The new TextView used to display the compass needle
+     */
     public void setTextView(TextView tv) {
         this.tv = tv;
     }
 
+    /**
+     * Returns the current TextView used to display the compass needle.
+     *
+     * @return The current TextView used to display the compass needle
+     */
     public TextView getTextView() {
         return tv;
     }
 
+    /**
+     * Updates the UI for the compass. It calculates the new angle for the compass needle, based on
+     * the location and orientation angles, and updates the position and rotation of the TextView
+     * used to display the compass needle accordingly.
+     */
     @Override
     public void updateUI(){
         float angle = locAngle + orientAngle;
