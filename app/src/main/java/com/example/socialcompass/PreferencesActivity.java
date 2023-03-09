8c58840ff099b7cf6ef3bf63f3a7d2d9e36fe1a2
glznn <gsm004@ucsd.edu>
2023-03-08 17:58:23 -0800

    added ROOM persistence for friend IDs and tests

--- app/src/main/java/com/example/socialcompass/PreferencesActivity.java
+++ app/src/main/java/com/example/socialcompass/PreferencesActivity.java
@@ -1,23 +1,36 @@
 package com.example.socialcompass;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 public class PreferencesActivity extends AppCompatActivity {
 
+    private FriendViewModel viewModel;
+    private Button addFriendButton;
+    private EditText newFriend;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferences);
+
+        viewModel = new ViewModelProvider(this)
+                .get(FriendViewModel.class);
+
+        this.newFriend = this.findViewById(R.id.editUUID);
+        this.addFriendButton = this.findViewById(R.id.add_Btn);
+
+        addFriendButton.setOnClickListener(this::onAddClicked);
     }
 
 
-    public void onSubmitClicked(View view) {
-        //starts intent to compass view activity
-        Intent intent = new Intent(this, CompassViewActivity.class);
-        startActivity(intent);
+    public void onAddClicked(View view) {
+        String uuid = findViewById(R.id.editUUID).toString();
+        newFriend.setText("");
+        viewModel.addFriend(uuid);
     }
 }
---
+++ app/src/main/java/com/example/socialcompass/idDao.java
@@ -0,0 +1,24 @@
+package com.example.socialcompass;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface idDao {
+    @Insert
+    long insert(userID id);
+
+    @Query("SELECT * FROM `user_ids` WHERE `id`=:id")
+    userID get(long id);
+
+    @Query("SELECT * FROM `user_ids`")
+    List<userID> getAll();
+
+    @Query("SELECT * FROM  `user_ids`")
+    LiveData<List<userID>> getAllLive();
+
+}
